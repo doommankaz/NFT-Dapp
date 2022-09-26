e8b5f1634b8bef57f6da22ec655fc3c0cf2be471
Duman <doommankaz@gmail.com>
2022-09-27 00:37:28 +0600

    Initial Commit

---
+++ harhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xdD45fD60711CE0c9e11eD31908b388Aa7c29A993";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
